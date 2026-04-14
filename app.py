7d256828304d77a86b98321c3e45bbb34f892a00
shotai12 <i.shota0812@gmai.com>
2026-04-14 23:35:20 +0900

    割り勘アプリ土台追加

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+def warikan(total, people):
+    return total / people
 
+total = int(input("合計金額を入力してください: "))
+people = int(input("人数を入力してください: "))
+
+result = warikan(total, people)
+print(f"一人あたり {result:.0f} 円です")
