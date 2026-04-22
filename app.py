da6d4395207d322e4004b4edd0ab7e830a723ea3
shotai12 <i.shota0812@gmail.com>
2026-04-23 08:56:50 +0900

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-def warikan(total, people):
-    return total / people
-
-total = int(input("合計金額を入力してください: "))
-people = int(input("人数を入力してください: "))
-
-result = warikan(total, people)
-print(f"一人あたり {result:.0f} 円です")
