a536c195f32b2d05870a80600610f12f0264393a
shotai12 <i.shota0812@gmail.com>
2026-04-24 09:23:03 +0900

    Initial Commit

---
+++ app_locker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app_locker
+//
+//  Created by sub on 2026/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app_locker/app_lockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_lockerApp.swift
+//  app_locker
+//
+//  Created by sub on 2026/04/24.
+//
+
+import SwiftUI
+
+@main
+struct app_lockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
